da55c130f6ffb4e145bc1f0610818586f4145554
Brian Olore <brian@olore.net>
2016-07-14 06:42:26 -0400

    New rule: input-requires-label - All inputs require a label

---
+++ test/rules/input-requires-label.spec.js
@@ -0,0 +1,75 @@
+/**
+ * MIT Licensed
+ */
+
+var expect  = require("expect.js");
+
+var HTMLHint  = require("../../index").HTMLHint;
+
+var ruleId = 'input-requires-label',
+    ruleOptions = {};
+
+ruleOptions[ruleId] = true;
+
+describe('Rules: '+ruleId, function(){
+
+    describe('Successful cases', function() {
+
+        it('Input tag with a matching label before should result in no error', function () {
+            var code = '<label for="some-id"/><input id="some-id" type="password" />';
+            var messages = HTMLHint.verify(code, ruleOptions);
+            expect(messages.length).to.be(0);
+        });
+
+        it('Input tag with a matching label after should result in no error', function () {
+            var code = '<input id="some-id" type="password" /> <label for="some-id"/>';
+            var messages = HTMLHint.verify(code, ruleOptions);
+            expect(messages.length).to.be(0);
+        });
+    });
+
+
+    describe('Error cases', function() {
+
+        it('Input tag with no matching label should result in an error', function () {
+            var code = '<input type="password">';
+            var messages = HTMLHint.verify(code, ruleOptions);
+            expect(messages.length).to.be(1);
+            expect(messages[0].rule.id).to.be(ruleId);
+            expect(messages[0].line).to.be(1);
+            expect(messages[0].col).to.be(7);
+            expect(messages[0].type).to.be('warning');
+        });
+
+        it('Input tag with label that doesn\'t match id should result in error', function () {
+            var code = '<input id="some-id" type="password" /> <label for="some-other-id"/>';
+            var messages = HTMLHint.verify(code, ruleOptions);
+            expect(messages.length).to.be(1);
+            expect(messages[0].rule.id).to.be(ruleId);
+            expect(messages[0].line).to.be(1);
+            expect(messages[0].col).to.be(7);
+            expect(messages[0].type).to.be('warning');
+        });
+
+        it('Input tag with blank label:for should result in error', function () {
+            var code = '<input id="some-id" type="password" /> <label for=""/>';
+            var messages = HTMLHint.verify(code, ruleOptions);
+            expect(messages.length).to.be(1);
+            expect(messages[0].rule.id).to.be(ruleId);
+            expect(messages[0].line).to.be(1);
+            expect(messages[0].col).to.be(7);
+            expect(messages[0].type).to.be('warning');
+        });
+
+        it('Input tag with no id should result in error', function () {
+            var code = '<input type="password" /> <label for="something"/>';
+            var messages = HTMLHint.verify(code, ruleOptions);
+            expect(messages.length).to.be(1);
+            expect(messages[0].rule.id).to.be(ruleId);
+            expect(messages[0].line).to.be(1);
+            expect(messages[0].col).to.be(7);
+            expect(messages[0].type).to.be('warning');
+        });
+    });
+
+});
